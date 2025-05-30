0c0e79919dfd9107439441a52b37bdc4f916ebea
aermatov <ermatovalymkul3@gmail.com>
2025-05-31 00:19:25 +0600

    add simple jwt

--- tasks/models.py
+++ tasks/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
 
     def __str__(self):
         return self.title
--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
-from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-
+from django.contrib import messages
+from django.contrib.auth import get_user_model, logout
+from django.template.context_processors import request
 from users.serializers import RegisterUserSerializer
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
 User = get_user_model()
 
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password1', 'password2', )
 
 class RegisterUserAPIView(APIView):
     def post(self, request, *args, **kwargs):
@@ -16,4 +30,28 @@ class RegisterUserAPIView(APIView):
             user = User(email=email)
             user.set_password(password)
             user.save()
-        return Response(True)
+        return Response({"message": "ПОЛЬЗОВАТЕЛЬ СОЗДАН"})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Регистрация прошла успешно!")
+            return redirect('index')
+        else:
+            print(form.errors)
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    authentication_form = AuthenticationForm
+
+
+def custom_logout_view(request):
+    logout(request)
+    return redirect('index')
